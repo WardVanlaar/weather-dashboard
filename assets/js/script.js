3696ee6a9c57cdd28a4ace61d383cc5e7612b0ba
Ward <wardv@tirf.ca>
2021-12-20 09:30:17 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+//https://webdesign.tutsplus.com/tutorials/build-a-simple-weather-app-with-vanilla-javascript--cms-33893
+//https://medium.com/@arifkabir/building-a-weather-app-using-javascript-css-1f05f160a3b0
