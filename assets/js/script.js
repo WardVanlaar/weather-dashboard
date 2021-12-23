a137554050eac0fe79f357920b1b4eba22555983
Ward <wardv@tirf.ca>
2021-12-23 16:13:05 -0500

    still working on linking search history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,7 +101,9 @@ form.addEventListener("submit", e => {
           colorCode(data2.current.uvi);
 
           }).catch(err => {
-            console.error('Failed to fetch - ' + urlInner);    
+            console.error('Failed to fetch - ' + urlInner);   
+            console.log(urlOuter);
+            console.log(urlInner);
         });
 
         }).catch(err => {
@@ -124,8 +126,6 @@ form.addEventListener("submit", e => {
       } else if (uvi >= 5) {
         $("UVI").children('span').addClass("severe");
     }  
-  
-  cityList.addEventListener("click", fetch(outerUrl));   
 }
 
 // save city
@@ -165,5 +165,92 @@ function cityListHandler(event) {
     } 
 }
 
-submit_btn.addEventListener("click", cityListHandler);
+// to retrieve weather data from cities in search history list
+function fetchWeatherData(event) {
+  
+  // e.preventDefault();
+  // const selectedCity = document.querySelector("#cityList");
+  // let inputVal = selectedCity.value;
 
+  for (var i = 0; i < cities.lenght; i++) {
+    const cities = JSON.parse(localStorage.getItem("cities")) || [];
+    let inputVal = cities.name[i].value;
+    console.log(inputVal);
+
+    const urlOuter = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    let urlInner = '';
+      
+    const resultPromise = fetch(urlOuter)
+        .then(responseO => responseO.json())
+        .then(data => {
+          const { main, coord, name, sys, weather, wind, dt } = data;
+          const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+            weather[0]["icon"]
+          }.svg`;
+          
+          const unixTime = data.dt;
+          const date = new Date(unixTime*1000);
+          const date1= date.toLocaleDateString("en-US");
+
+          const neededValue1 = data.coord.lon;
+          const neededValue2 = data.coord.lat;
+
+          urlInner = `https://api.openweathermap.org/data/2.5/onecall?lat=${neededValue2}&lon=${neededValue1}&exclude=hourly,daily&appid=${apiKey}`;
+          return fetch(urlInner)
+            .then(responseI => responseI.json())
+            .then(data2 => {
+              const {current} = data2;
+              // localStorage.setItem("uvi", JSON.stringify(data2));
+              // console.log(data2);
+
+              city.textContent = "";
+
+              const li = document.createElement("span");
+              li.classList.add("city");
+              const markup = `
+                <h2 class="city-name" data-name="${name},${sys.country}">
+                  <span>${name}, ${sys.country} (${date1}) <figure> <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}"></figure>
+                  </span>
+                </h2>
+                <div class="city-temp">Temp: ${Math.round(main.temp)}<sup>°</sup>C</div>
+                <div class="city-wind">Wind: ${wind.speed} Km/h</div>
+                <div class="city-humidity">Humidity: ${main.humidity}%</div>
+                <div class="city-UVI" id="UVI"> UV index: <span>${current.uvi}</span></div>
+              `;
+
+              li.innerHTML = markup;
+              list.appendChild(li);
+              
+              colorCode(data2.current.uvi);
+
+              }).catch(err => {
+                console.error('Failed to fetch - ' + urlInner);   
+                console.log(urlOuter);
+                console.log(urlInner);
+            });
+
+            }).catch(err => {
+              console.error('Failed to fetch - ' + urlOuter);
+          });
+        };
+
+        // const uvi = JSON.parse(localStorage.getItem("uvi")) || [];
+        // console.log(uvi);
+
+        function colorCode(uvi) {
+          console.log("uvi value inside cc f", uvi)
+        
+          // uvi.removeClass(".favorable .moderate .severe");
+
+          if (uvi < 2) {
+            $("#UVI").children('span').addClass("favorable");
+          } else if ((uvi >= 2) & (uvi < 5)) {
+            $("UVI").children('span').addClass("moderate");
+          } else if (uvi >= 5) {
+            $("UVI").children('span').addClass("severe");
+        }
+      }    
+  }
+
+submit_btn.addEventListener("click", cityListHandler);
+cityList.addEventListener("click", fetchWeatherData);
