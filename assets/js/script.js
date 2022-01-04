ce09953a4a63ced2f2afffb7ca141bf789d5806c
Ward <wardv@tirf.ca>
2022-01-04 14:43:15 -0500

    added code to reload city from search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,6 +149,7 @@ function cityListHandler(event) {
      
       var cityEl = document.createElement("button");
       cityEl.classList = "btn btn-secondary list-item";
+      cityEl.setAttribute("id", "cities" + i);
       var titleEl = document.createElement("span");
       titleEl.textContent = cities[i].name;
       cityEl.appendChild(titleEl);
@@ -156,11 +157,78 @@ function cityListHandler(event) {
     } 
 }
 
+// // To load city from search history
+// function fetchWeatherData(cityEl) {
+//   const urlOuter = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+//   let urlInner = '';
 
-// // to retrieve weather data from cities in search history list
-// function fetchWeatherData(event) {
+//   const resultPromise = fetch(urlOuter)
+//     .then(responseO => responseO.json())
+//     .then(data => {
+//       const { main, coord, name, sys, weather, wind, dt } = data;
+//       const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+//         weather[0]["icon"]
+//       }.svg`;
+      
+//       const unixTime = data.dt;
+//       const date = new Date(unixTime*1000);
+//       const date1= date.toLocaleDateString("en-US");
 
+//       const neededValue1 = data.coord.lat;
+//       const neededValue2 = data.coord.lon;
+
+//       urlInner = `https://api.openweathermap.org/data/2.5/onecall?lat=${neededValue1}&lon=${neededValue2}&appid=${apiKey}`;
+//       return fetch(urlInner)
+//         .then(responseI => responseI.json())
+//         .then(data2 => {
+//           const {current, daily} = data2;
+//           // localStorage.setItem("uvi", JSON.stringify(data2));
+//           // console.log(data2);
+
+//           city.textContent = "";
+
+//           const li = document.createElement("span");
+//           li.classList.add("city");
+//           const markup = `
+//             <h2 class="city-name" data-name="${name},${sys.country}">
+//               <span>${name}, ${sys.country} (${date1}) <figure> <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}"></figure>
+//               </span>
+//             </h2>
+//             <div class="city-temp">Temp: ${Math.round(main.temp)}<sup>°</sup>C</div>
+//             <div class="city-wind">Wind: ${wind.speed} Km/h</div>
+//             <div class="city-humidity">Humidity: ${main.humidity}%</div>
+//             <div class="city-UVI" id="UVI"> UV index: <span>${current.uvi}</span></div>
+//           `;
+
+//           li.innerHTML = markup;
+//           list.appendChild(li);
+          
+//           colorCode(data2.current.uvi);
+//           loadForecast(data2);
+
+//           }).catch(err => {
+//             console.error('Failed to fetch - ' + urlInner);   
+//             console.log(urlOuter);
+//             console.log(urlInner);
+//         });
+
+//         }).catch(err => {
+//           console.error('Failed to fetch - ' + urlOuter);
+//       });
+//     };
+
+//     function colorCode(uvi) {
+//         console.log(uvi)
+      
+//       if (uvi < 2) {
+//         $("#UVI").children('span').addClass("favorable");
+//       } else if (uvi > 5) {
+//         $("#UVI").children('span').addClass("severe");
+//       } else {
+//         $("#UVI").children('span').addClass("moderate");
+//       }
+//   cityEl.addEventListener("click", fetchWeatherData);
 // }
 
 submit_btn.addEventListener("click", cityListHandler);
-// cityList.addEventListener("click", fetchWeatherData);
+
